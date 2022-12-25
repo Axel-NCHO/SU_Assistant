715bdd96e7b607a713d6364375d212d6098dab2c
Axel_N'CHO <naxelamour09+github@gmail.com>
2022-12-25 08:22:10 +0100

    Network finished + external interface 1/2 separated from outputInterface

---
+++ Brain/Patterns.py
@@ -0,0 +1,10 @@
+patterns = {"fr":
+                {"pattern_photo": [{"LEMMA": "prendre", "POS": "VERB", "OP": "*"}, {"POS": "NOUN", "OP": "*"},
+                                   {"LOWER": "photo"}]
+                 },
+
+            "en":
+                {"pattern_photo": [{"LEMMA": "take", "POS": "VERB", "OP": "*"}, {"POS": "NOUN", "OP": "*"},
+                                   {"LOWER": "photo"}]
+                 }
+            }
